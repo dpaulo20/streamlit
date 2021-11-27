57f335586b6a7174aac15c11e1ab61a203105afd
dias <npdiaspereira@gmail.com>
2021-11-27 10:45:07 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Image Classification with Google's Teachable Machine")
+
+st.header("Brain Tumor MRI Classification Example")
+
+st.text("Upload a brain MRI Image for image classification as tumor or no-tumor")
