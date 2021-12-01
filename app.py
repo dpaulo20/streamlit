80a9f87cf63193677d8a359422877dcc2620c7a1
dias <npdiaspereira@gmail.com>
2021-12-01 21:22:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -250,13 +250,8 @@ if image_path is not None:
      with graph3.as_default():
          prediction_class_VGG16 = model_VGG16.predict(data_C)
             
-     class_dict = {0: 'Fish', 1: 'Flower', 2: 'Gravel', 3: 'Sugar'}
-     cols = st.columns(4) 
-     for i in range(4):
-         title='class  '+class_dict[i]
-         cols[i].header(prediction_class_VGG16[i])
 
-   #  st.text(prediction_class_VGG16)
+     st.header(prediction_class_VGG16)
         
 
 
